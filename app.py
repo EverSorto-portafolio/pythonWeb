55c954a361f189414a031b3473f64bf77830853a
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-03-30 16:54:27 -0600

    GUIA 1 Se añade SQLAlchemy

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask , request
+
+app = Flask(__name__)
+
+@app.route('/')
+def  home():
+    return "Hola mundo"
+
+
+
+@app.route('/create-article', methods=['GET', 'POST'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        return f'Articulo creado {title}, contenido: {content}'
+    return """
+    <form method ='POST' action = "create-article">
+        <label for ='title'> Titulo</label>
+        <input type = 'text' id = 'title' name = 'title'>
+        <br>
+        <label for ='content'> Contenido</label>
+        <input type = 'text' id = 'content' name = 'content'>
+        <br>
+        <input type = 'submit' value = 'Crear articulo'>
+    </form>
+"""
+
+
+
+@app.route('/article/<int:article_id>')
+def view_article( article_id):
+    return (f"Ver artículo {article_id}")      
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
