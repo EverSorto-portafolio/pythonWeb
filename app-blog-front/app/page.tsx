50f3d90a66e788a1c55982b5bc66d8e2ed356fe0
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-05-19 22:12:33 -0600

    guia 3 se agrega la vista con react aparecen las card

--- app-blog-front/app/page.tsx
+++ app-blog-front/app/page.tsx
@@ -4,7 +4,7 @@ import Layout from "./components/Layout";
 
 
 
-interface Article{
+interface Article {
   id: number;
   title: string;
   content: string;
@@ -14,18 +14,33 @@ export default function Home() {
   const [article, setArticle] = useState<Article[]>([]);
   useEffect(() => {
     fetch("http://127.0.0.1:5000/articles")
-    .then(response => response.json())
-    .then(data => setArticle(data))
-    .catch(error => console.error("Error fetching articles:", error));
+      .then(response => response.json())
+      .then(data => setArticle(data))
+      .catch(error => console.error("Error fetching articles:", error));
   }, []);
-console.log(article);
+  console.log(article);
 
   return (
-    <Layout>
-      <div className="container mx-auto py-10">
-        <h1 className="text-4xl font-bold md-6 text-indigo-600">Hola mundo</h1>
+    <>
+      <Layout>
+        <div className="container mx-auto py-10">
+          <h1 className="text-4xl font-bold md-6 text-indigo-600">Hola mundo</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {
+              article.map((article) => (
+                <div key={article.id} className="bg-white rounded-lg shadow-lg p-6 hover:shadow-xl transition-shadow duration-300">
+                  <h2 className="text-2xl  font-bold text-indigo-600 mb-2" >
+                    {article.title}
+                  </h2>
+                  <p>{article.content.slice(0, 100)}...</p>
+                  <a href={`/article/${article.id}`} className="text-indigo-600 font-semibold hover:text-indigo-400"> Leer mas</a>
+                </div>
+              ))
 
-      </div>
-    </Layout>
+            }
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 }
